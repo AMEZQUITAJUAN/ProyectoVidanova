8b9c51b4f6b10913ee77822cae01c78914c08b54
AMEZQUITAJUAN <amezquitadussan@gmail.com>
2025-08-05 12:30:30 -0500

    base2

--- vida_nova/indicadores/models.py
+++ vida_nova/indicadores/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class board(models.Model):
     name = models.CharField(max_length=100)
@@ -23,19 +24,20 @@ class Patient(models.Model):
     age = models.IntegerField()
     vital_status = models.CharField(max_length=10)
 
-    class MedicalRequest(models.Model):
-        case_type = models.CharField(max_length=100)                # Tipo de Caso
-    intake_date = models.DateField()                            # Fecha de captación
-    procedure_type = models.CharField(max_length=100)           # Tipo de procedimiento
-    request_status = models.CharField(max_length=100)           # Estado de la solicitud
-    request_date = models.DateField()                           # Fecha de solicitud
-    appointment_date = models.DateField()                       # Fecha de cita
-    provider = models.CharField(max_length=150)                 # Prestador
+class MedicalRequest(models.Model):
+    case_type = models.CharField(max_length=100,blank=True, null=True)                # Tipo de Caso
+    intake_date = models.DateField(blank=True, null=True)                            # Fecha de captación
+    procedure_type = models.CharField(max_length=100,blank=True, null=True)           # Tipo de procedimiento
+    request_status = models.CharField(max_length=100,blank=True, null=True)           # Estado de la solicitud
+    request_date = models.DateField(blank=True, null=True)      # Fecha de solicitud
+    appointment_date = models.DateField(default=timezone.now)                       # Fecha de cita
+    provider = models.CharField(max_length=150,blank=True, null=True)                 # Prestador
     barrier = models.TextField(blank=True, null=True)           # Barrera (zona rural, transporte, etc.)
     observations = models.TextField(blank=True, null=True)      # Observaciones
-    timeliness = models.IntegerField()                          # Oportunidad (días entre solicitud y cita, por ejemplo)
-    care_pathway = models.CharField(max_length=100)             # Ruta
-    month = models.CharField(max_length=20)                     # Mes
+    timeliness = models.IntegerField(blank=True, null=True)     # Oportunidad (días entre solicitud y cita, por ejemplo)
+    care_pathway = models.CharField(max_length=100, blank=True, null=True)             # Ruta
+    month = models.CharField(max_length=20, blank=True, null=True)                     # Mes
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.case_type} - {self.intake_date}"
